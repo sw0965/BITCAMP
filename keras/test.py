68c7f008122350b16ae4e3ff2353904efd9d3701
sw0965 <hsw0965@naver.com>
2020-05-22 09:42:26 +0900

    master

---
+++ keras/test.py
@@ -0,0 +1,13 @@
+from numpy import array
+
+x1 = array([1,2,3,4,5,6])
+x2 = array([[1,2,3],[4,5,6]])
+x3 = array([[1],[2],[3],[4],[5]])
+x4 = array([[[1],[2]],[[2],[3]]])
+
+y = array([1,2,3,4,5,6,7,8])
+
+print('x1.shape : ', x1.shape)
+print('x2.shape : ', x2.shape)
+print('x3.shape : ', x3.shape)
+print('x4.shape : ', x4.shape)
