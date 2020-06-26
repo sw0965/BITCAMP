b63658a7065d02a6fafd36efa2accc36722e78cc
sw0965 <hsw0965@naver.com>
2020-06-26 17:53:51 +0900

    master

--- BEAK_STUDY/self_beak.py
+++ BEAK_STUDY/self_beak.py
@@ -109,9 +109,70 @@
 #------------- 알람 시계 문제 (브3)-------------#
 # import datetime
 # H, M = map(int,input().split())
-# if 0 <= H <= 23 and 0 <= M <= 59:
-#     if :
+#--실 패--
 
+# ls = [H,M]
+#     if M-45<0 and H>0:
+#         print(H-1, M+15)
+
+#     elif M-45<0 and H==0:
+#         print(H+23,M+15)
+
+#     elif M-45>0 and H==0:
+#         print(H,M)
+
+#     elif M-45 > 0:
+#         print(H,M)
+
+#     elif M-45 == 0:
+#         print(H, M-45)
+
+# if 0 <= H <= 23 and 0 <= M <= 59:
+
+
+#--실 패--
+# if a < 0:
+#     print(H-1,a+60)
+# elif (a < 0 == H -1) < 0:
+#     print(H+)
+#     b = H-1
+#     if b<0:
+
+# elif a > 0:
+#     print(H,a)
+# elif H-1<0:
+#     print(24-H-1)
+
+# -실패-
+# if M-45<0 and H>0:
+#     print(H-1, M+15)
+
+# elif M-45<0 and H==0:
+#     print(H+23,M+15)
+
+# elif M-45>0 and H==0:
+#     print(H,M)
+
+# elif M-45 > 0:
+#     print(H,M)
+
+# elif M-45 == 0:
+#     print(H, M-45)
+
+# 성공
+# if M-45<0 and H>0:
+#     H = H-1
+#     M = M+15
+
+# elif M-45<0 and H==0:
+#     H = H+23
+#     M = M+15
+
+# elif M-45>=0:
+#     H = H
+#     M = M-45
+
+# print(H, M)
 
 # https://www.acmicpc.net/problem/2558
 #------------- A+B - 2 문제 (브5)-------------#
@@ -120,5 +181,79 @@
 # print(A+B)
 # 0<A, B<10
 
+# https://www.acmicpc.net/problem/5522
+#------------- 카드 게임 문제 (브5)-------------#
+# a1 = int(input())
+# a2 = int(input())
+# a3 = int(input())
+# a4 = int(input())
+# a5 = int(input())
+# ls=[a1, a2, a3, a4, a5]
+# print(sum(ls))
+
+# https://www.acmicpc.net/problem/5554
+#------------- 심부름 가는 길 문제 (브5)-------------#
+# S = int(input())
+# P = int(input())
+# A = int(input())
+# H = int(input())
+# ls = [S, P, A, H]
+# print('%d \n%d' %(sum(ls)/60, sum(ls)%60))
+
+# https://www.acmicpc.net/problem/2739
+#------------- 구구단  문제 (브5)-------------#
+# N = int(input())
+
+# a = list(range(1, 10))
+# for i in a:
+#     print('%d * %d = %d' %(N, i, N*i))
+
+# https://www.acmicpc.net/problem/10950
+#------------- A+B - 3  문제 (브3)-------------#
+# T = int(input())
+# for i in range(T):
+#     A, B = map(int,input().split())
+#     print(A+B)
+
+# https://www.acmicpc.net/problem/15552
+#------------- 빠른 A+B  문제 (브2)-------------#
+#-정답-#
+# import sys
+# T = int(sys.stdin.readline())
+# for i in range(T):
+#     A, B = map(int,sys.stdin.readline().split())
+#     print(A+B)
+#-실패-
+# import sys
+# for i in sys.stdin.readline():
+#     print(i)
+
+# https://www.acmicpc.net/problem/2741
+#------------- N 찍기  문제 (브3)-------------#
+# import sys
+# N = int(sys.stdin.readline())
+# for i in list(range(1,N+1)):
+#     print(i)
+
+# 다른예시
+# import sys
+# N = sys.stdin.readline()
+# for i in list(range(1,int(N)+1)):
+#     print(i)
+
+# https://www.acmicpc.net/problem/2742
+#------------- 기찍 N  문제 (브3)-------------#
+import sys
+N = sys.stdin.readline()
+for i in list(N[::-1]):
+    print(i)
+
+'''
 # MIN, MAX = map(int,input().split())
 # if 1 <= MIN <= 1000000000000 and MIN <= MAX <= MIN+1000000:
+a = (1, 2, 3, 4, 5, 6)
+for i in
+
+a = '4'
+print
+'''
--- BEAK_STUDY/submit_1.py
+++ BEAK_STUDY/submit_1.py
@@ -1,8 +1,11 @@
-#------------- 나머지 문제 (브5)-------------#
-A, B, C = map(int,input().split())
-print((A+B)%C)
-print(((A%C) + (B%C))%C)
-print((A*B)%C)
-print(((A%C)*(B%C))%C)
+# https://www.acmicpc.net/problem/5522
+#------------- 카드 게임 문제 (브5)-------------#
+a1 = int(input())
+a2 = int(input())
+a3 = int(input())
+a4 = int(input())
+a5 = int(input())
+ls=[a1, a2, a3, a4, a5]
+print(sum(ls))
 
-# 어제유형에서 그냥 프린트 문만 찍어줘도 되어 무난하게 진행하였습니다.
+# sum 함수를 쓰기 위해 리스트에 넣어준뒤 프린트 하였습니다.
